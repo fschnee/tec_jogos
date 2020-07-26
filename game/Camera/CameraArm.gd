7182df7570ef13c71e048c5c5d529ae390f4aed6
Frederico Schaun <fbdsschaun@inf.ufpel.edu.br>
2020-07-26 02:08:15 -0300

    refactor(camera): makes camera arm update only when necessary

--- game/Camera/CameraArm.gd
+++ game/Camera/CameraArm.gd
@@ -1,15 +1,38 @@
 tool
 extends Spatial
 
-export (Array, NodePath) var lerp_targets
+export (Array, NodePath) var lerp_targets setget set_lerp_targets
 var _lerp_targets: Array
+
+func set_lerp_targets(new_lerp_targets):
+	lerp_targets = new_lerp_targets
+	recalc_targets()
+
+func recalc_targets():
+	_lerp_targets.clear()
+	for t in lerp_targets:
+		_lerp_targets.append(get_node(t))
 
 export var follow_speed = 2
 export var rotate_speed = 6
-export var distance = 20
+export (float) var distance = 20 setget set_camera_distance
+export (float, -90, 0) var camera_angle = -45 setget set_camera_angle
+
+func set_camera_distance(newdist):
+	distance = newdist
+	recalc_camera()
+func set_camera_angle(newangle):
+	camera_angle = newangle
+	$Camera.set_rotation_degrees(Vector3(camera_angle, 0, 0))
+	recalc_camera()
+func recalc_camera():
+	# Set the camera _distance_ away (basic trig).
+	var y_dist = sin(abs($Camera.get_rotation().x)) * distance
+	var z_dist = sqrt(distance * distance - y_dist * y_dist)
+	$Camera.set_translation(Vector3(0, y_dist, z_dist))
 
 var target_rot = 0
-var rotations = [
+const rotations = [
 	Quat(Vector3.UP, deg2rad(45)),
 	Quat(Vector3.UP, deg2rad(135)),
 	Quat(Vector3.UP, deg2rad(225)),
@@ -17,18 +40,11 @@ var rotations = [
 ]
 
 func _ready():
-	for t in lerp_targets:
-		_lerp_targets.append(get_node(t))
+	recalc_camera()
+	recalc_targets()
 
-# TODO: reposition camera only when $Camera.get_rotation().x or 
-# distance changes instead of at every _physics_process.
 func _physics_process(delta):
-	# Set the camera _distance_ away (basic trig).
-	var y_dist = sin(abs($Camera.get_rotation().x)) * distance
-	var z_dist = sqrt(distance * distance - y_dist * y_dist)
-	$Camera.set_translation(Vector3(0, y_dist, z_dist))
-
-	# Then rotate to the arm to the right angle
+	# Rotate to the arm to the right angle
 	target_rot = wrapi(
 		(
 			target_rot +
